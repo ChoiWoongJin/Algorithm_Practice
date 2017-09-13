dee77eaf759a90405f46def51ce6fe66e18de7b3
asus <asus@DESKTOP-MPDMUUP>
2017-09-13 19:24:10 +0900

    Solved_Problem

--- JapanProblem/problem/Japan/choi/GetNumOutStar.java
+++ JapanProblem/problem/Japan/choi/GetNumOutStar.java
@@ -1,5 +1,24 @@
 package problem.Japan.choi;
 
-public class GetNumOutStar {
+import java.util.Scanner;
 
+/**
+ * 입력받은 숫자 만큼 별을 출력하는 코드
+ */
+
+public class GetNumOutStar {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+
+        Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+        int num = Integer.parseInt(sc.nextLine());
+        
+        for(int i=0; i<num; i++) {
+            sb.append("*");
+        }        
+        
+        System.out.println(sb.toString());
+    }
 }
