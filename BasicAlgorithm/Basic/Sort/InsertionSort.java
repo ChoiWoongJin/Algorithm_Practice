11af808e3e95d4c05eb8f0e08da9de13fafd66da
ChoiWoongJin <musi0436@naver.com>
2017-11-10 19:27:06 +0900

    Insertion Sort - 삽입 정렬

---
+++ BasicAlgorithm/Basic/Sort/InsertionSort.java
@@ -0,0 +1,40 @@
+package Basic.Sort;
+
+/* 오름차순 정렬일 때 : 1부터 시작하여 n까지 순차적으로 정렬해 나간다
+ * 시간복잡도 – O( n² )
+ */
+
+public class InsertionSort {
+	public static int[] selectionSort(int[] arr) {
+		int temp = 0, tempPoint = 0;
+		for(int i=1; i < arr.length; i++) {
+			tempPoint = i;
+			for(int j=i-1; j >= 0; j--) {
+				if(arr[tempPoint] < arr[j]) {
+					temp = arr[tempPoint];
+					arr[tempPoint] = arr[j];
+					arr[j] = temp;
+					tempPoint = j;
+				} else {
+					break;
+				}
+			}
+		}
+		
+		return arr;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {29, 10, 14, 37, 13};
+		
+		for(int i=0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+		
+		selectionSort(arr);		
+		for(int i=0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}		
+	}
+}
