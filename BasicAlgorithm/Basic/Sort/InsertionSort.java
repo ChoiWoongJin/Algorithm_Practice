e2e5795df2bce408649027cd19fb969fae5c7c03
ChoiWoongJin <musi0436@naver.com>
2017-11-10 19:41:51 +0900

    insertion Sort - 삽입 정렬

--- BasicAlgorithm/Basic/Sort/InsertionSort.java
+++ BasicAlgorithm/Basic/Sort/InsertionSort.java
@@ -32,7 +32,7 @@ public class InsertionSort {
 		}
 		System.out.println();
 		
-		selectionSort(arr);		
+		insertionSort(arr);		
 		for(int i=0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}		
